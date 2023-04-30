0dab5411533751fc4476a2b4f08bd12c365e69b8
warhogz <90264787+warhogz@users.noreply.github.com>
2023-05-01 00:06:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const dog = document.getElementById("dog");
+const brevno = document.getElementById("brevno");
+
+document.addEventListener("keydown", function(event){
+    jump()
+});
+
+function jump(){
+    if(dog.classList != "jump"){
+        dog.classList.add("jump")
+    }
+    setTimeout( function(){
+        dog.classList.remove("jump")
+    }, 300)
+}
+
+let isAlive = setInterval (function(){
+    let dogTop = parseInt(window.getComputedStyle(dog).getPropertyValue("top")) 
+    let brevnoLeft = parseInt(window.getComputedStyle(brevno).getPropertyValue("left"))
+    if (brevnoLeft <50 && brevnoLeft > 0  && dogTop >= 140){
+        alert("GAME OVER")
+        location.reload()
+
+    }
+}, 10)
