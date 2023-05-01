8119c03afb5c7d44a0481f36925223ffb3b0250e
warhogz <90264787+warhogz@users.noreply.github.com>
2023-05-01 05:27:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const dog = document.getElementById("dog");
 const brevno = document.getElementById("brevno");
+const div = document.createElement("div");
+div.classList.add('black');
+document.body.append(div);
 
 document.addEventListener("touchstart", function(event){
     jump()
@@ -14,12 +17,17 @@ function jump(){
     }, 300)
 }
 
+var score = 0;
 let isAlive = setInterval (function(){
     let dogTop = parseInt(window.getComputedStyle(dog).getPropertyValue("top")) 
     let brevnoLeft = parseInt(window.getComputedStyle(brevno).getPropertyValue("left"))
+    if(brevno[i].x==30){
+        score++;
+    }
     if (brevnoLeft <50 && brevnoLeft > 0  && dogTop >= 140){
         alert("GAME OVER")
         location.reload()
 
     }
 }, 10)
+div.innerHTML = ("Ваш счет: " + score);
